0bfc88ce6de30c93b3a89e9e82792ca96781ad1e
Ifeoma N. <31158530+IfyN@users.noreply.github.com>
2025-07-13 13:06:37 +0000

    feature: add button stories files

--- my-component-library/src/components/Button.jsx
+++ my-component-library/src/components/Button.jsx
@@ -1,17 +1,28 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { StyledButton } from "./Button.styles";
 
-export default function Button ({label, variant = "Primary", onClick, ...props}){
+export default function Button({
+  label,
+  variant = "primary",
+  size = "md",
+  onClick,
+  ...props
+}) {
   return (
-    <StyledButton $variant={variant} onClick={onClick} {...props}>
-    {label}
-  </StyledButton>
-
-)}
-
-Button.PropTypes = {
-  label: PropTypes.string.isRequired,
-  variant: PropTypes.oneOf(["primary", "secondary"]),
-  onClick: PropTypes.func,
+    <StyledButton
+      $variant={variant}
+      $size={size}
+      onClick={onClick}
+      {...props}
+    >
+      {label}
+    </StyledButton>
+  );
 }
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf(["primary", "secondary", "outline"]),
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
+  onClick: PropTypes.func,
+};
---
+++ my-component-library/src/stories/Button.stories.jsx
@@ -0,0 +1,46 @@
+import Button from "../components/Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+};
+
+export const Primary = {
+  args: {
+    label: "Primary Button",
+    variant: "primary",
+    size: "md",
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: "Secondary Button",
+    variant: "secondary",
+    size: "md",
+  },
+};
+
+export const Outline = {
+  args: {
+    label: "Outline Button",
+    variant: "outline",
+    size: "md",
+  },
+};
+
+export const Large = {
+  args: {
+    label: "Large Button",
+    variant: "primary",
+    size: "lg",
+  },
+};
+
+export const Small = {
+  args: {
+    label: "Small Button",
+    variant: "primary",
+    size: "sm",
+  },
+};
